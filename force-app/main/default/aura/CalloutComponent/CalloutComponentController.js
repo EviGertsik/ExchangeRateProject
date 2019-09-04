9d01ce2434f296da6f689ba4ca373cea6a48ba82
evigertsik@gmail.com <evigertsik@gmail.com>
2019-09-04 11:43:23 +0300

    Initial commit

---
+++ force-app/main/default/aura/CalloutComponent/CalloutComponentController.js
@@ -0,0 +1,36 @@
+({
+    myAction : function(component, event, helper) {
+        
+        var startDate = component.find('StartDate').get('v.value');
+        var endDate = component.find('EndDate').get('v.value');
+        var baseCurrency = component.find('BaseCurrency').get('v.value');
+        var action = component.get("c.RateCallout");
+        action.setParams({
+            "StartDate": startDate,
+            "EndDate": endDate,
+            "BaseCurrency": baseCurrency
+        })
+        action.setCallback(this, function(Response){
+            var state = Response.getState();
+            if (state === "SUCCESS"){
+                var result = Response.getReturnValue();
+                if (result){
+                    result = JSON.parse(result);
+                    console.log(result);
+                   // Map<String,Map<String,Map<String,Number>>> m =  
+                //    var map = Object.entries(result.rates);
+                //    console.log(map[0][1]);
+                //    console.log(map.values());
+                $A.enqueueAction(action);
+                }
+                
+            }
+            // for (const iterator of map) 
+            //     console.log(iterator [1])
+            })
+        
+        
+        
+    
+
+    }})
